332442cff26fc24fad60480006bebfa7326e314b
Libby Li <libby@Libby.local>
2018-09-14 15:07:27 -0500

    Here's my commit message

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.swift
 //  HelloWorld
 //
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
+    var backgroundColor: UIColor!
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        backgroundColor = view.backgroundColor
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-}
+    @IBAction func didTapButton(_ sender: Any) {
+        textLabel.textColor = UIColor.orange
+    }
+    
+    @IBAction func didTapViewButton(_ sender: Any) {
+        view.backgroundColor = UIColor.yellow
+    }
+    
+    @IBAction func didTapTextButton(_ sender: Any) {
+        if textField.hasText == false {
+            textLabel.text = "Goodbye!"
+        }
+        else {
+            textLabel.text = textField.text
+            textField.text = ""
+            view.endEditing(true)
+        }
+    }
+    
+    @IBAction func onResetGesture(_ sender: Any) {
+        textLabel.text = "Hello from Charlie!"
+        textLabel.textColor = UIColor.black
+        view.backgroundColor = backgroundColor
+    }
+ }
 
